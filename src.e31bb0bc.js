a65337e99c68a449e030752cc753f987c148c1d8
serhii.fedorenko90@gmail.com <serhii.fedorenko90@gmail.com>
2021-06-02 19:30:24 +0300

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -2546,7 +2546,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51497" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53235" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
